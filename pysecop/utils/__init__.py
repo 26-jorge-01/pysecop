dcff9b5f373f0819f1b4e75bac13aaf8938d49da
26-jorge-01 <jonan0804@gmail.com>
2026-04-23 11:18:39 -0500

    chore: initialize utils package and update test coverage data

--- pysecop/utils/__init__.py
+++ pysecop/utils/__init__.py
@@ -1,3 +1,3 @@
-from .helpers import normalize_dataframe, get_search_filters, consolidate_dataframes, get_unified_columns
+from .helpers import normalize_dataframe, get_search_filters, consolidate_dataframes, get_unified_columns, get_mapped_column
 from .logging import get_logger
 from .normalizer import SmartNormalizer
